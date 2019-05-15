46770052e59d0224782dda258c543d18d731cd7d
Jason <j.grant.brown@gmail.com>
2019-05-15 12:26:25 -0400

    stubbing out divesites controller

--- app/controllers/api/divesites_controller.rb
+++ app/controllers/api/divesites_controller.rb
@@ -2,4 +2,22 @@ class Api::DivesitesController < ApplicationController
   def index
     render json: Divesites.all
   end
+
+  def create
+    divesite = Divesites.new(divesite_params)
+    if divesite.save
+      render json: divesite
+    else
+      render json: {message: divesite.errors}, status: 400
+    end
+  end
+
+  def show
+      @divesite = Divesites.find_by(id: params[:id])
+    render json: @divesite
+  end
+
+  def divesites_params
+    params.require(:divesites).permit(:name, :location , :img_ur, :description)
+  end
 end
